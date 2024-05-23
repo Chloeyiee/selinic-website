438816249ddb959542ca6901be7a5fc88e76f400
Chloeyiee <126537831+Chloeyiee@users.noreply.github.com>
2024-05-24 04:14:25 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// var typed= new typed(".text", {
+//     String:["Students", "Data Analyst", "Data Scientist"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay:1000,
+//     loop:true
+// });
